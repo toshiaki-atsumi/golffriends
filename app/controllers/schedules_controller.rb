c0410313897936d8eec2c4c516a86102fd74bb39
toshiaki.atsumi <toshiaki.atsumi@techacademy.jp>
2019-07-22 08:04:43 +0000

    2019.7.22

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,8 +1,12 @@
 class SchedulesController < ApplicationController
+  before_action :require_organaizer_logged_in
+  before_action :require_party_selected
+  
   def index
   end
 
   def create
+    
   end
 
   def edit
@@ -17,3 +21,26 @@ class SchedulesController < ApplicationController
   def destroy
   end
 end
+
+  include SessionsHelper
+  include PartiesHelper
+  
+  private
+  
+  def require_organaizer_logged_in
+     unless organizer_logged_in?
+       redirect_to root_url
+     end  
+  end 
+  
+  def require_member_logged_in
+    unless logged_in?
+      redirect_to login_url
+    end
+  end
+  
+  def require_party_selected
+    if $current_party == nil
+       redirect_to root_url
+    end
+  end
