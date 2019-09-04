b44e3e9e5713c93bea4f3e3b9fda0712c1a6bfcd
toshiaki.atsumi <toshiaki.atsumi@techacademy.jp>
2019-09-04 08:06:50 +0000

    2019.9.4

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,10 +1,9 @@
 class ParticipantsController < ApplicationController
+  
   def index
-   
-    @schedules = Schedule.where(party_id: $current_party.id).where('date > ?',Date.today).page(params[:page]).per(10)
-    
+     @schedules = Schedule.where(party_id: $current_party.id).where('date > ?',Date.today).page(params[:page]).per(10)
   end
-
+  
   def show
   end
 
