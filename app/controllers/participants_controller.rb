b85e95245bbdf8c2d3ddde31b603553c84c40ffe
toshiaki.atsumi <toshiaki.atsumi@techacademy.jp>
2019-09-07 02:57:45 +0000

    2019.9.7

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -11,9 +11,22 @@ class ParticipantsController < ApplicationController
   end
 
   def create
-    binding.pry
+    params[:p_id].each_with_index do | reg ,i|
+      if reg == "0"
+        @participant=Participant.new
+        @participant.schedule_id = params[:s_id][i] 
+        @participant.member_id = current_member.id
+        @participant.mark = params[:marks][i]
+        @participant.comment = params[:comments][i]
+        @participant.save
+      end
+    end
   end
 
   def edit
+  end
+  
+  def participant_params
+    params.permit(:p_id,:s_id,:marks,:comment)
   end
 end
